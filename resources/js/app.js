67b075514ae737624d79d249ff340c0f593a7c54
Mohamed Ramadan <m7mdrama1245@gmail.com>
2021-04-02 06:04:42 +0200

    Finish Apollo manual setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,17 +3,32 @@ import VueRouter from "vue-router";
 import VueResource from "vue-router";
 import "./bootstrap";
 // import { routes } from "./routes";
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
+
 import PostList from "./PostList";
 import Post from "./Post";
 
 window.Vue = Vue;
-Vue.use(VueRouter);
 
+
+Vue.use(VueRouter);
 const routes = [
     { path: "/", name: "index", component: PostList },
     { path: "/post/:id", name: "post", component: Post }
     // { path: "*", redirect: "/" }
 ];
+
+
+Vue.use(VueApollo);
+const apolloClient = new ApolloClient({
+    // You should use an absolute URL here
+    uri: "http://127.0.0.1:8000/graphql"
+});
+
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient
+});
 
 const router = new VueRouter({
     mode: "history",
@@ -22,5 +37,6 @@ const router = new VueRouter({
 
 const app = new Vue({
     el: "#app",
+    apolloProvider,
     router
 });
